7ff53266fdd7af01d15c6ed9c82de701adc9d4a4
Jeshuwa Bradley <76804175+JeshuwaBradley@users.noreply.github.com>
2021-12-29 11:19:03 +0530

    Second commit

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, {useState } from 'react'
 import './navbar.css'
 import { FiMenu, FiX } from 'react-icons/fi';
 
 
  
 const Navbar = ({navbarLinks}) => {
+    const [menuClicked, setMenuClicked] = useState(false);
+
+    const toggleMenuClick = () => {
+        setMenuClicked(!menuClicked);
+    };
     return (
         <nav className='navbar'>
             <span className='navbarLogo'>Travel</span>
-            <ul className='navbarList'>
+            {menuClicked ? (
+                <FiX size={25} className={"navbarMenu"} onClick={toggleMenuClick} />
+            ) : (
+                <FiMenu
+                size={25}
+                className={"navbarMenu"}
+                onClick={toggleMenuClick}
+                />
+            )}
+            <ul className={
+                    menuClicked ? "navbarList navbarList--active" : "navbarList"
+                }
+            >
                 {navbarLinks.map((item) => (
                     <li className='navbarItem'>
                         <a className='navbarLink' href={item.url}>
